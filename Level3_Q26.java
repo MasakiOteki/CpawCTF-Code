89341727c8b5613c31ac2db1e4641bd2db21630f
MasakiOteki <it232060@trident.ac.jp>
2024-05-30 19:55:52 +0900

    Update Level3_Q26.java
    インデントが汚かったので修正しました。

--- Level3_Q26.java
+++ Level3_Q26.java
@@ -1,10 +1,10 @@
 // package
 public class Level3_Q26{
-public static void main(String[] args) {
-    long num = 30000;
-    while(num%1584891!=32134 || num%3438478!=193127){
-        num++;
+    public static void main(String[] args) {
+        long num = 30000;
+        while(num%1584891!=32134 || num%3438478!=193127){
+            num++;
+        }
+        System.out.println(num);
     }
-    System.out.println(num);
-}
 }
